f821b0c1fb4c980280ad2c6b53fb6e711e254c9b
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-24 19:31:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do 
-  end get '/name' do 
-  end get '/name' do 
+  end
+  get '/hometown' do 
+  end 
+  get '/favorite-song' do 
   end
   
 end
